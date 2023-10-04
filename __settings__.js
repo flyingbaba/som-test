49123a8e97d0b73ab1cae5609cef13e95cc03fc0
Max\max <maxDubovsky@gmail.com>
2023-10-04 13:58:37 +0300

    second commit

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1871952.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 152469913, 152469921, 152469923, 152469937, 152469940, 152469947, 152469949, 152469950 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/152469906/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/152469907/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/152469905/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/152469910/1/basis.wasm.js', 'wasmUrl' : 'files/assets/152469911/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/152469909/1/basis.js', 'preload' : false},
+];
